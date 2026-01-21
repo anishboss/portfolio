872bf45ef96372308d798b002345bc2411bffb7d
dianomax <anishbudhathoki50@gmail.com>
2026-01-21 12:58:38 +0545

    add impportmetaenv

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_PAGES_BASE_PATH: string;
+  // add more custom env variables here if needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
