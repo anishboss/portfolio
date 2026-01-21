da2a36a896d3132bd3413ba55b5cfb838bb70bb3
dianomax <anishbudhathoki50@gmail.com>
2026-01-21 12:50:54 +0545

    update workflow and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const gitHubPagesBasePath = process.env.PAGES_BASE_PATH;
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  ...(gitHubPagesBasePath ? { basePath: gitHubPagesBasePath } : {}),
   server: {
     open: true,
     port: 3000,
