e05f4ce20f769616bc0a9e298b6983192f660845
dianomax <anishbudhathoki50@gmail.com>
2026-01-21 13:02:18 +0545

    base portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // base: gitHubPagesBasePath ? gitHubPagesBasePath : '/',
-  base: '/',
+  base: '/portfolio/',
   server: {
     open: true,
     port: 3000,
