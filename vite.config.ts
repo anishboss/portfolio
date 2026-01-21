5272275140b4eff374becf746ee837e837c89a64
dianomax <anishbudhathoki50@gmail.com>
2026-01-21 13:01:52 +0545

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const gitHubPagesBasePath = import.meta.env.VITE_PAGES_BASE_PATH;
+// const gitHubPagesBasePath = import.meta.env.VITE_PAGES_BASE_PATH;
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: gitHubPagesBasePath ? gitHubPagesBasePath : '/',
+  // base: gitHubPagesBasePath ? gitHubPagesBasePath : '/',
+  base: '/',
   server: {
     open: true,
     port: 3000,
